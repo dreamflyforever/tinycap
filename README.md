fa81048e24a9f9dfccb92b26c9aec9917d6fbd25
jim <1150660744@qq.com>
2022-01-06 10:51:29 +0800

    first version
    Signed-off-by: jim <1150660744@qq.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+## tinycap
+Use for no playback device like hw:0,0, accroding to /dev/snd/pcmDXCXc capture sound.
+
+## license
+modify by jim
---
+++ aarch_compile.sh
@@ -0,0 +1,1 @@
+aarch64-linux-gnu-gcc pcm.c tinycap.c -I include  -L /home/jim/workspace/ai/speech/libasound2/alsa-lib_1.2.2/src/.libs -lasound -o capture
